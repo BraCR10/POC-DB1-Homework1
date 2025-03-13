47c40b5d6230c80b4b13dac597e168452b187ca3
BraCR10 <brianramirez01arias@gmail.com>
2025-03-13 14:14:36 -0600

    adding initial config

--- db-poc-api/src/app.ts
+++ db-poc-api/src/app.ts
@@ -1,14 +1,24 @@
 import express, { Request, Response } from "express";
-
+import { initConnection } from "./config/db.config";
+import Router from "./routes";
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("¡Hi, Express API!");
-});
+app.use("/api",Router);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+
+const startServer = async () => {
+  try {
+
+   await initConnection();
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error details: ", error);
+    process.exit(1); 
+  }
+};
+
+startServer();
