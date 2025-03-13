d3f291a9f7ef044ff13c78de6ffe23998ba81ca6
BraCR10 <brianramirez01arias@gmail.com>
2025-03-13 12:34:04 -0600

    Adding Gets User Services

--- db-poc-api/src/services/Employ.service.ts
+++ db-poc-api/src/services/Employ.service.ts
@@ -32,3 +32,42 @@ export async function createEmploy(
     throw new Error(`An error occurred while creating the employ: ${error}`);
   }
 }
+
+
+export async function getEmploys(): Promise<Employ[]> {
+  try {
+    const response = await query("getEmploys", {});
+
+    if (response.recordset.length > 0) {
+      return response.recordset;
+    } else {
+      throw new Error("No employs found");
+    }
+  } catch (error) {
+    console.error("Error details:", error);
+    throw new Error(`An error occurred while fetching the employs: ${error}`);
+  }
+}
+
+export async function getEmployById(id: number): Promise<Employ> {
+    if (!id || id < 1) {
+        throw new Error("Invalid id");
+    }
+    
+    const params: SqlParameters = {
+        id: [id.toString(), TYPES.Int],
+    };
+    
+    try {
+        const response = await query("getEmployById", params);
+    
+        if (response.recordset.length > 0) {
+        return response.recordset[0];
+        } else {
+        throw new Error("Employ not found");
+        }
+    } catch (error) {
+        console.error("Error details:", error);
+        throw new Error(`An error occurred while fetching the employ: ${error}`);
+    }
+    }
