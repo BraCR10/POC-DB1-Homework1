92eff7eedc448310b90b2f48edd3248575e285d0
BraCR10 <brianramirez01arias@gmail.com>
2025-03-17 13:02:08 -0600

    Adding dashboard style

--- db-poc-ui/src/app/components/RowCard.tsx
+++ db-poc-ui/src/app/components/RowCard.tsx
@@ -10,15 +10,16 @@ interface RowCardProps {
 function RowCard({ id, name, salary , className }: RowCardProps) {
   return (
     <div className={`row-${className ? className : ""}`}>
+      <div className="id">
+        <h1>{id}</h1>
+      </div>
       <div className="name">
         <h1>{name}</h1>
       </div>
       <div className="salary">
         <h1>{salary}</h1>
       </div>
-      <div className="id">
-        <h1>{id}</h1>
-      </div>
+      
     </div>
   );
 }
