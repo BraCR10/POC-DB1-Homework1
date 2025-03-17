790114bcea3e9427511a39c9a71d2c7047d68285
BraCR10 <brianramirez01arias@gmail.com>
2025-03-17 00:01:24 -0600

    Get employees services

---
+++ db-poc-ui/src/app/services/getEmployees.ts
@@ -0,0 +1,24 @@
+export interface Employee {
+  ID: number;
+  Name: string;
+  Salary: number;
+}
+
+export async function getEmployees(): Promise<Employee[]> {
+  try {
+    console.log("Fetching employee data");
+    const response = await fetch(
+      "https://employeesapi-beta.vercel.app/api/employee",
+    );
+    if (!response.ok) {
+      console.log(response);
+      throw new Error("Employee data not found");
+    }
+    const employees = await response.json();
+    console.log("Employee data found");
+    return employees.data;
+  } catch (e) {
+    console.log("Employee data not found ${e}");
+    throw Error("Employee data not found ${e}");
+  }
+}
