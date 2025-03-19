d6c6efe7f249f57a6cb86cdb6971bef030a432f4
BRACR10 <Brianramirez01arias@gmail.com>
2025-03-19 15:06:31 -0600

    Deleting attemping process

--- db-poc-api/src/services/Employee.service.ts
+++ db-poc-api/src/services/Employee.service.ts
@@ -25,43 +25,19 @@ class EmployeeService {
   }
 
   async getEmployees(): Promise<Employee[]> {
-    const maxRetries = 5;
-    let retryCount = 0;
-    let delay = 1000;
-
-    while (retryCount < maxRetries) {
-      try {
-        const response = await query("sp_get_all_employees", {});
-        if (response.recordset) {
-          return response.recordset;
-        } else {
-          return [];
-        }
-      } catch (error) {
-        retryCount++;
-
-        if (retryCount >= maxRetries) {
-          console.error("Error many attempts:", error);
-          throw new Error(
-            `An error occurred after ${maxRetries} attempts: ${error}`,
-          );
-        }
-
-        console.log(
-          `Attempt ${retryCount}/${maxRetries} failed, re-trying ${delay}ms...`,
-        );
-        await new Promise((resolve) => setTimeout(resolve, delay));
-        delay *= 2;
+    try {
+      const response = await query("sp_get_all_employees", {});
+      if (response.recordset) {
+        return response.recordset;
+      } else {
+        return [];
       }
+    } catch (error) {
+      throw new Error("Error fetching the data.");
     }
-    throw new Error("No employees data returned from the database.");
   }
 
   async getEmployeeById(id: number): Promise<Employee[]> {
-    const maxRetries = 5;
-    let retryCount = 0;
-    let delay = 1000;
-
     if (!id || id < 1) {
       throw new Error("Invalid id");
     }
@@ -70,65 +46,29 @@ class EmployeeService {
       id: [id.toString(), TYPES.Int],
     };
 
-    while (retryCount < maxRetries) {
-      try {
-        const response = await query("sp_get_employee_by_id", params);
-        if (response.recordset && response.recordset.length > 0) {
-          return response.recordset[0];
-        } else {
-          throw new Error("Employee not found");
-        }
-      } catch (error) {
-        retryCount++;
-
-        if (retryCount >= maxRetries) {
-          console.error("Error many attempts:", error);
-          throw new Error(
-            `An error occurred after ${maxRetries} attempts: ${error}`,
-          );
-        }
-
-        console.log(
-          `Attempt ${retryCount}/${maxRetries} failed, re-trying ${delay}ms...`,
-        );
-        await new Promise((resolve) => setTimeout(resolve, delay));
-        delay *= 2;
+    try {
+      const response = await query("sp_get_employee_by_id", params);
+      if (response.recordset && response.recordset.length > 0) {
+        return response.recordset[0];
+      } else {
+        throw new Error("Employee not found");
       }
+    } catch (error) {
+      throw new Error("Error fetching the data.");
     }
-    throw new Error("No employee data returned from the database.");
   }
 
   async getEmployeesSortedByName(): Promise<Employee[]> {
-    const maxRetries = 5;
-    let retryCount = 0;
-    let delay = 1000;
-
-    while (retryCount < maxRetries) {
-      try {
-        const response = await query("sp_get_all_employees", {});
-        if (response.recordset) {
-          return response.recordset.sort((a, b) => a.Name.localeCompare(b.Name));
-        } else {
-          return [];
-        }
-      } catch (error) {
-        retryCount++;
-
-        if (retryCount >= maxRetries) {
-          console.error("Error many attempts:", error);
-          throw new Error(
-            `An error occurred after ${maxRetries} attempts: ${error}`,
-          );
-        }
-
-        console.log(
-          `Attempt ${retryCount}/${maxRetries} failed, re-trying ${delay}ms...`,
-        );
-        await new Promise((resolve) => setTimeout(resolve, delay));
-        delay *= 2;
+    try {
+      const response = await query("sp_get_all_employees", {});
+      if (response.recordset) {
+        return response.recordset.sort((a, b) => a.Name.localeCompare(b.Name));
+      } else {
+        return [];
       }
+    } catch (error) {
+      throw new Error("Error fetching the data.");
     }
-    throw new Error("No employees data returned from the database.");
   }
 }
 
